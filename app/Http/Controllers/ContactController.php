e9cdf4d599a06762003c23e4be73ddce903dfa8f
JOSE <bmonkdareg@gmail.com>
2022-06-21 23:11:29 -0400

    Middleware auth

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,6 +9,12 @@ use Inertia\Inertia;
 
 class ContactController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
